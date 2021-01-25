b3c91ff7df52b5c05ec80ee67175502d7ee41ff8
marhaensalenindokomara <marhaensa@gmail.com>
2021-01-25 19:28:56 +0700

    terus terang production ver 0.2

--- js/script.js
+++ js/script.js
@@ -1,87 +1,113 @@
-/*!
-    * Start Bootstrap - Agency v6.0.3 (https://startbootstrap.com/theme/agency)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-    */
-   (function ($) {
-    "use strict"; // Start of use strict
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length
-                ? target
-                : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top - 72,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
-                return false;
-            }
-        }
-    });
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function () {
-        $(".navbar-collapse").collapse("hide");
-    });
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#mainNav",
-        offset: 74,
-    });
-    // Collapse Navbar
-    var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
-})(jQuery); // End of use strict
-// VISI MISI
-$(window).on('load',function(){
-    $('.pLeft').addClass('pShow');
-    $('.pRight').addClass('pShow');
+(function ($) {
+  "use strict";
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 72,
+          },
+          1000,
+          "easeInOutExpo"
+        );
+        return false;
+      }
+    }
+  });
+  $(".js-scroll-trigger").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: 74,
+  });
+  var navbarCollapse = function () {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  navbarCollapse();
+  $(window).scroll(navbarCollapse);
+})(jQuery);
+$(window).on("load", function () {
+  $(".pLeft").addClass("pShow");
+  $(".pRight").addClass("pShow");
 });
-$(window).scroll(function(){
-    var wScroll = $(this).scrollTop();
-    //Layanan
-    if(wScroll > $('.layanan').offset().top-120){
-        $('.layanan .timeline-panel').each(function(i){
-            setTimeout(function(){
-                $('.layanan .timeline-panel').eq(i).addClass('show');
-            },220*(i+1));
-        });
-        
-    }
-    // Portfolio
-    if(wScroll > $('.portfolio').offset().top-120){
-        $('.portfolio .portfolio-item').each(function(i){
-            setTimeout(function(){
-                $('.portfolio .portfolio-item').eq(i).addClass('show');
-            },220*(i+1));
-        });
-        
-    }
-    // Product
-    if(wScroll > $('.product').offset().top-120){
-        $('.product .team-member').each(function(i){
-            setTimeout(function(){
-                $('.product .team-member').eq(i).addClass('show');
-            },220*(i+1));
-        });
-        
-    }
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop();
+  if (wScroll > $(".layanan").offset().top - 120) {
+    $(".layanan .timeline-panel").each(function (i) {
+      setTimeout(function () {
+        $(".layanan .timeline-panel").eq(i).addClass("show");
+      }, 220 * (i + 1));
+    });
+  }
+  if (wScroll > $(".portfolio").offset().top - 120) {
+    $(".portfolio .portfolio-item").each(function (i) {
+      setTimeout(function () {
+        $(".portfolio .portfolio-item").eq(i).addClass("show");
+      }, 220 * (i + 1));
+    });
+  }
+  if (wScroll > $(".product").offset().top - 120) {
+    $(".product .team-member").each(function (i) {
+      setTimeout(function () {
+        $(".product .team-member").eq(i).addClass("show");
+      }, 220 * (i + 1));
+    });
+  }
 });
+
+// ! JSON
+
+// ? IMG PORTFOLIO
+function imgList() {
+  $.getJSON("json/index.json", function (data) {
+    let url = data.url;
+    // console.log(url);
+    $.each(url, function (i, data) {
+      $("#img-list").append(
+        `
+        <div class="col-lg-3">
+        <img src="assets/img/portfolio-adi/` +
+          data.url +
+          `" alt="" width="200px" class="photo">
+        </div>   `
+      );
+    });
+  });
+}
+imgList();
+
+function klienList() {
+  $.getJSON("json/klien.json", function (data) {
+    let url = data.url;
+    console.log(url);
+    $.each(url, function (i, data) {
+      $("#list-klien").append(
+        `
+        <div class="col-md-3 col-sm-6 my-3">
+        <a href="#!"
+          >
+          <img width="150px"
+            class="img-fluid d-block mx-auto"
+            src="assets/img/logos/` +
+          data.url +
+          `"
+            alt=""
+        /></a>
+      </div>  `
+      );
+    });
+  });
+}
+
+klienList();
